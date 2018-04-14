2af016e267c81138806acbd002c278387a0388be
Nick Murray <Nick Murray>
2018-04-14 16:09:38 -0500

    functioning MVC - moving on to add Google News

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		artist_input = request.form['artist-entry'] #name attribute of the form in the view, gets the term the user searched on.
-		artist = model.search_artists(artist_input)[0][1]#First artist in search results
-		print("PRINTING ARTIST")
-		print(artist)
-		overview = model.get_wiki_page(artist)
-		related = model.get_others_in_genre(artist) #request data with functions defined in the model
-	else:
-		artist = "Danny Brown"
-		overview = model.get_wiki_page(artist)
-		related = model.get_others_in_genre(artist)
-		print(overview)
-	return render_template('index.html',artist=artist,related=related)
+	try:
+		if request.method == 'POST':
+			artist_input = request.form['artist-entry'] #name attribute of the form in the view, gets the term the user searched on.
+			artist = model.search_artists(artist_input)[0][1]#First artist in search results
+			print("PRINTING ARTIST")
+			print(artist)
+			overview = model.get_wiki_page(artist)
+			related = model.get_others_in_genre(artist) #request data with functions defined in the model
+		else:
+			artist = "Danny Brown"
+			overview = model.get_wiki_page(artist)
+			related = model.get_others_in_genre(artist)
+			print(overview)
+		return render_template('index.html',artist=artist,related=related,overview=overview)
+	except:
+		return render_template('error.html')
 
 
 
