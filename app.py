5f7cd260ebdca59170677b3b2b139559b1a513db
Nick Murray <Nick Murray>
2018-04-17 11:46:02 -0500

    UI stopping point

--- app.py
+++ app.py
@@ -28,15 +28,15 @@ clear_cache()
 def related_display(artist): 
 	related_data = model.get_others_in_genre(artist) #request data with functions defined in the model 
 	related_dict = {}
-	for item in related_data:
-		related_dict[item.name] = item.artist_url
+	for item in related_data[:5]: #Only top 5
+		related_dict[item.name] = item.image_url
 	return related_dict
 
 #Tracks
 def track_display(artist):
 	track_data = model.get_top_tracks(artist)
 	track_dict = {}
-	for item in track_data:
+	for item in track_data[:5]: #Only top 5
 		track_dict[item.name] = item.track_url
 	return track_dict
 
@@ -44,7 +44,7 @@ def track_display(artist):
 def article_display(artist):
 	article_data =  model.get_headlines(artist)
 	article_dict = {}
-	for item in article_data:
+	for item in article_data[:5]:#Only top 5
 		article_dict[item.title] = item.url
 	return article_dict
 
@@ -58,28 +58,33 @@ def staging():
 #Landing Page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	try:
-		if request.method == 'POST':
-			artist_input = request.form['artist-entry'] #name attribute of the form in the view, gets the term the user searched on.
-			artist = model.search_artists(artist_input)[0].name#First artist in search results
-
-			overview = model.get_wiki_page(artist)
-			related = related_display(artist)
-			tracks = track_display(artist)
-			articles = article_display(artist)
-				
-		else:
-			artist = "Danny Brown"
-			overview = model.get_wiki_page(artist)
-			related = related_display(artist)
-			tracks = track_display(artist)
-			articles = article_display(artist)
+	# try:
+	if request.method == 'POST':
+		artist_input = request.form['artist-entry'] #name attribute of the form in the view, gets the term the user searched on.
+		artist_result = model.search_artists(artist_input)[0]#First artist in search results
+		artist_name = artist_result.name
+		image_url = artist_result.image_url
 
 
+		overview = model.get_wiki_page(artist_name)
+		related = related_display(artist_name)
+		tracks = track_display(artist_name)
+		articles = article_display(artist_name)
+			
+	else:
+		artist_result = model.search_artists("Danny Brown")[0]#
+		artist_name = artist_result.name
+		image_url = artist_result.image_url
+		overview = model.get_wiki_page(artist_name)
+		related = related_display(artist_name)
+		tracks = track_display(artist_name)
+		articles = article_display(artist_name)
 
-		return render_template('index.html',artist=artist,related=related,overview=overview,tracks=tracks,articles=articles)
-	except:
-		return render_template('error.html')
+
+
+	return render_template('index.html',artist_name=artist_name,image_url=image_url,related=related,overview=overview,tracks=tracks,articles=articles)
+	# except:
+		# return render_template('error.html')
 
 
 
